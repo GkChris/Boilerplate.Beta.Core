cd9f25d315e4b49faeb89ad176f1ccc35d422220
unknown <gkagkaschristos@gmail.com>
2025-09-13 13:47:20 +0300

    Add example value object to and ValueObjects folder to models

---
+++ Boilerplate.Beta.Core/Application/Models/ValueObjects/AddressExampleValueObject.cs
@@ -0,0 +1,8 @@
+﻿namespace Boilerplate.Beta.Core.Application.Models.ValueObjects
+{
+    public class AddressExampleValueObject
+	{
+		public string Street { get; set; }
+		public string City { get; set; }
+	}
+}
